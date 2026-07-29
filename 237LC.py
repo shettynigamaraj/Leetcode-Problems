bbf031501c2ab94508c556af93904a1081d76cf2
shettynigamaraj <shettynigamaraj@gmail.com>
2026-07-29 20:25:24 +0530

    Update 237LC.py

--- 237LC.py
+++ 237LC.py
@@ -10,7 +10,7 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        node.val=node.next.valx
+        node.val=node.next.val
         node.next=node.next.next
                 
                 #why we did this is , bcz , we are not given the head only , so we need not traverse the entire list, and we should'nt delete it in the form of delete method , instead make the node same as nextnode and thats how the connection of previous and the current node happens
