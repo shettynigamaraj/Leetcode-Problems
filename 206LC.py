762f3e60d65ba1ef113b392aad40de5a713c74e9
shettynigamaraj <shettynigamaraj@gmail.com>
2026-07-30 21:17:39 +0530

    Create 206LC.py

---
+++ 206LC.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            nxt = curr.next      # Save next node
+            curr.next = prev     # Reverse the link
+            prev = curr          # Move prev forward
+            curr = nxt           # Move curr forward
+
+        return prev
