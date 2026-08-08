7e2c40370313ebdee6a71e5b46abf7ba2899f7c6
shettynigamaraj <shettynigamaraj@gmail.com>
2026-08-08 23:30:21 +0530

    Create 1290LC.py

---
+++ 1290LC.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        a=""
+        while head:
+            a+=str(head.val)
+            head=head.next
+        return int(a,2)
